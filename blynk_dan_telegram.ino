55d290561f12af6fa718ee660d4e93b62657682c
pastibisa106 <35872007+pastibisa106@users.noreply.github.com>
2025-05-05 06:16:22 +0700

    Add files via upload

---
+++ blynk_dan_telegram.ino
@@ -0,0 +1,61 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+#include "DHT.h"
+
+#define DHTPIN D1
+
+char ssid[] = "raqhamedia"; //nama wifi
+char password[] = "apayalupa"; //password wifi
+#define BOTtoken "1127735094:AAG3NxYtcNA4M1YBhuXiKuJCOyXw8v968pY" //token bot telegram
+#define idChat "326286458" //idbot
+
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOTtoken, client);
+
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.print("Connecting Wifi: ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  dht.begin();
+  delay(5000);
+}
+
+void loop() {
+  delay(2000);
+  
+  float t = dht.readTemperature();
+  Serial.print("Suhu : ");
+  Serial.print(t);
+  Serial.println(" *C");
+  
+  if (t > 30.00) {
+    bot.sendChatAction(idChat, "Sedang mengetik...");
+    Serial.print("Suhu saat ini : ");
+    Serial.print(t);
+    Serial.println(" *C");
+    delay(3000);
+
+    String suhu = "Intensitas suhu : ";
+    suhu += int(t);
+    suhu += " *C\n";
+    suhu += "Suhu Over..!\n";
+    bot.sendMessage(idChat, suhu, "");
+    Serial.print("Mengirim data sensor ke telegram");
+  }
+}
